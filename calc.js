fd34573e6cfc0891ad78a4178fd160992a3458cf
Brian Talbert <BrianLTalbert@gmail.com>
2022-11-04 14:21:16 -0400

    Finalize calculator logic

--- calc.js
+++ calc.js
@@ -13,7 +13,8 @@ function addButtonEventListeners() {
             let num = ''
             id == 'dec' ? num = '.' : num = id
             button.addEventListener('click', button => {
-                //if screen only shows 0, clear and add the number (if it's not a decimal)
+                //if screen only shows 0, clear and add the number.
+                //if beginning with a decimal, start with 0.
                 if (screen.textContent == '0' || screen.textContent == '') {
                     screen.textContent = ''
                     if (num != '.') {screen.textContent += num} else {screen.textContent = '0.'}
@@ -44,6 +45,22 @@ function addButtonEventListeners() {
                         op = 'eq'
                         break
                 }
+
+/*                 //allow subtraction operator to be used to make
+                //a number negative
+                if (op == '-') {
+                    switch (operands.length) {
+                        case 0:
+                            screen.textContent = op
+                            break;
+                        case 1:
+                            break;
+                        case 2:
+                            screen.textContent += op
+                            break;
+                    }
+                } */
+
                 //if this is the first operand, add it to the list
                 //if they press equals, don't do anything
                 if (!operands[0] && op != 'eq') {
@@ -53,10 +70,15 @@ function addButtonEventListeners() {
 
                 //else if there is an operand and an operation
                 } else if (operands.length == 2){
-                    operands.push(+screen.textContent.split(operands[1])[1])
+                    operands.push(+screen.textContent.split(operands[1], 2)[1])
                     result = operate()
                     operands = [result]
                     screen.textContent = result
+                    if (op != 'eq') {
+                        operands.push(op)
+                        screen.textContent += op
+                    }
+                    
                     console.log(operands)
                 } else if (operands.length == 1) {
                     operands.push(op)
